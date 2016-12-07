955123e331696f5c44a0beea5c91520c36eccb5a
Shanie Zhang <shanshan_amelian@hotmail.com>
2016-12-07 14:59:00 -0500

    Commit change
    Commit Change

--- app/src/main/java/com/hidecontacts4/HideContactsModule.java
+++ app/src/main/java/com/hidecontacts4/HideContactsModule.java
@@ -30,18 +30,20 @@ public class HideContactsModule implements IXposedHookLoadPackage {
     ArrayList<String> results = new ArrayList<>();
     static {
 
-        procMap.put("Shanshan Zhang", "Dummy2");
-        procMap.put("Zan", "Dummy3");
+        procMap.put("Shanshan Zhang", "Dummy1");
+        procMap.put("Zan", "Dummy2");
         procMap.put("1", "0");   //same for numbers
         procMap.put("6","111");
         procMap.put("3","222");
          //ditto
         procMap.put("(301)385-7492", "(222)222-2222");   //ditto
 
-        susPackagesForFake.add("com.shanshan.simplecontacts"); //
+        susPackagesForFake.add("com.shanshan.simplecontacts");
+        susPackagesForFake.add("com.simpler.contacts");
+
 //   like this:     susPackagesForFake.add("")
 
-//        susPackagesForNull.add("")  // TODO: add these
+       //susPackagesForNull.add();  // TODO: add these
     }
 
     @Override
@@ -143,6 +145,51 @@ public class HideContactsModule implements IXposedHookLoadPackage {
 
     private void hookQueryForNull(XC_LoadPackage.LoadPackageParam lpparam) {
         // TODO:
+        final Class<?> cResolver = findClass("android.content.ContentResolver",
+                lpparam.classLoader);
+        XposedBridge.hookAllMethods(cResolver, "query", new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                String uri = param.args[0].toString().toLowerCase();
+                XposedBridge.log("Intercepted uri is = " + uri);
+                for (int i = 0; i < param.args.length; i++) {
+                    //XposedBridge.log(i + "-th arg: " + param.args[i]);
+                }
+                if (uri.contains("com.android.contacts") && !uri.contains("profile")) {
+                    Uri URI = (Uri) param.args[0];
+
+                    Cursor cursor = (Cursor) param.getResult();
+                    int ColumeIndex_ID = cursor.getColumnIndex(ContactsContract.Contacts._ID);
+                    int ColumeIndex_DISPLAY_NAME = cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
+                    int ColumeIndex_HAS_PHONE_NUMBER = cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER);
+                    XposedBridge.log("Cursor number is = " + cursor.getCount());
+                    if (cursor != null && cursor.getCount() > 0) {
+                        cursor.moveToFirst();
+                        while (cursor.moveToNext()) {
+                            List<String> listColumn = new ArrayList<>();
+
+                            listColumn.addAll(Arrays.asList(cursor.getColumnNames()));
+                            String id = cursor.getString(ColumeIndex_ID);
+                            String name = cursor.getString(ColumeIndex_DISPLAY_NAME);
+                            String has_phone = cursor.getString(ColumeIndex_HAS_PHONE_NUMBER);
+
+                            XposedBridge.log("id is "+ id+ "+name is" + name + "+has phone number is" + has_phone);
+
+
+                        }
+                    } else {
+                        XposedBridge.log("Cursor is empty");
+                    }
+
+                    List<String> listColumn = new ArrayList<>();
+                    listColumn.addAll(Arrays.asList(cursor.getColumnNames()));
+                    // XposedBridge.log(result.toString());
+                    for (int i = 0; i <= listColumn.size() - 1; i++) {
+                        // XposedBridge.log("column is" + listColumn.get(i).toString());
+                    }
+                }
+            }
+        });
     }
 
     /*private void hookCursor(XC_LoadPackage.LoadPackageParam lpparam,
@@ -175,6 +222,28 @@ public class HideContactsModule implements IXposedHookLoadPackage {
 
     private void hookCursorForNull(XC_LoadPackage.LoadPackageParam lpparam, final String clazz, final String methodName) {
         // TODO:
+        findAndHookMethod(clazz, lpparam.classLoader, methodName, int.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                // TODO: use MainActivity to extend, use name or index to check
+                String realName = param.getResult().toString();
+                if (procMap.containsKey(realName)) {
+                    XposedBridge.log(String.format("method %s get called, let's return null",
+                            methodName, realName));
+                    //XposedBridge.log(String.format("replaced real value %s by %s",
+                    // realName, procMap.get(realName)));
+                    if (methodName.equalsIgnoreCase("getString")) {
+
+                        param.setResult(null);
+                    } else if (methodName.equalsIgnoreCase("getInt")) {
+                        param.setResult(null);
+                    }
+                }else{
+
+                    XposedBridge.log(String.format("method %s get called, the real value does not need to be protected %s", methodName, realName));
+                }
+            }
+        });
     }
 }
 
